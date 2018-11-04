5e0d2cd256e6f77d76833dc5dce7e57c038e921b
ssong-eu <sangyoon.song@gmail.com>
2018-11-04 12:35:00 +0000

    Done.

--- src/crewMember.js
+++ src/crewMember.js
@@ -1,1 +1,30 @@
-class CrewMember {}
+class CrewMember {
+  constructor (position) {
+    this.position = position
+    this.currentShip = "Looking for a Rig"
+  };
+
+  engageWarpDrive () {
+    if (this.position === "Pilot" && this.currentShip !== "Looking for a Rig") {
+      this.currentShip.warpDrive = "engaged"
+    } else if (this.currentShip === "Looking for a Rig") {
+      return "had no effect"
+    };
+  };
+
+  setsInvisibility () {
+    if (this.position === "Defender" && this.currentShip !== "Looking for a Rig") {
+      this.currentShip.cloaked = true
+    } else if (this.currentShip === "Looking for a Rig") {
+      return "had no effect"
+    };
+  };
+
+  chargePhasers () {
+    if (this.position === "Gunner" && this.currentShip !== "Looking for a Rig") {
+      this.currentShip.phasersCharge = "charged"
+    } else if (this.currentShip === "Looking for a Rig") {
+      return "had no effect"
+    };
+  };
+}
