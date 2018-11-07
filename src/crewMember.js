a4949609ae6d69e4d70a9fa572aaf8d178371526
Patrick <ctrlaltpat@gmail.com>
2018-11-07 11:22:51 +0000

    Done.

--- src/crewMember.js
+++ src/crewMember.js
@@ -1,1 +1,31 @@
-class CrewMember {}
+class CrewMember {
+  constructor(position, currentShip = "Looking for a Rig") {
+    this.position = position
+    this.currentShip = currentShip
+  }
+
+  engageWarpDrive() {
+    if(this.currentShip === "Looking for a Rig") {
+      return "had no effect"
+    } else if (this.position.toLowerCase() === 'pilot'){
+      this.currentShip.warpDrive = this.currentShip.warpDrive === 'engaged' ? "disengaged" : 'engaged'
+    }
+  }
+
+  setsInvisibility() {
+    if(this.currentShip === "Looking for a Rig") {
+      console.log(this.position)
+      return "had no effect"
+    } else if (this.position.toLowerCase() === 'defender'){
+      this.currentShip.cloaked = true
+    }
+  }
+
+  chargePhasers() {
+    if(this.currentShip === "Looking for a Rig") {
+      return "had no effect"
+    } else if (this.position.toLowerCase() === 'gunner'){
+      this.currentShip.phasersCharge = this.currentShip.phasersCharge === 'charged' ? 'uncharged' : 'charged'
+    }
+  }
+}
